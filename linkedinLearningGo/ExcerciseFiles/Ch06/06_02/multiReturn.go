8e41f5620c27a3b12c5e40d32d9f3da6931da116
mcclayac <anthony.mcclay@me.com>
2020-08-19 04:26:28 -0400

    Some changes

--- linkedinLearningGo/ExcerciseFiles/Ch06/06_02/multiReturn.go
+++ linkedinLearningGo/ExcerciseFiles/Ch06/06_02/multiReturn.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 
+	nm, le := FullName("Tony", "McClay")
+
+	fmt.Printf("full name :%v length: %v", nm, le)
+}
+
+func FullName(f, l string) (string, int) {
+
+	full := f + " " + l
+	length := len(full)
+	return full, length
 }
